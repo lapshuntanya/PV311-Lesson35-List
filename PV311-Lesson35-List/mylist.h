e8fa41052d0ea25a5822aca7d57b261d05e4b992
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-27 20:14:51 +0300

    update list

--- PV311-Lesson35-List/mylist.h
+++ PV311-Lesson35-List/mylist.h
@@ -15,5 +15,31 @@ namespace MyList {
 
 		}
 
+		void push_back(U value) {
+			//1 Створюємо вузол
+			Node<U>* el = new Node<U>(value);
+
+			//Якщо це перший вузол
+			if (head == nullptr) {
+				head = el;//Назначаємо голову та хвіст
+				tail = el;
+			}
+			else {//Якщо НЕ перший вузол
+				tail->next = el;
+				tail = el;//Оновлюємо хвіст
+			}
+		}
+
+		void showInfo()const {
+			if (head == nullptr) cout << "list is empty\n";
+			else {
+				Node<U>* p = head;
+				while (p != nullptr)
+				{
+					p->show();
+				}
+			}
+		}
+
 	};
 }
