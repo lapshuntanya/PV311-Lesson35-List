21f258889738bed3c5bff787d808376f975dc95b
lapshuntanya <lapshun.tatiana@gmail.com>
2024-06-27 20:44:00 +0300

    class DNode

--- PV311-Lesson35-List/Node.h
+++ PV311-Lesson35-List/Node.h
@@ -18,4 +18,25 @@ namespace MyList {
 			cout << "\tnext: " << next << endl;
 		}
 	};
+
+	template<typename T>
+	class DNode
+	{
+	public:
+		T item;
+		DNode* next;
+		DNode* prev;
+
+		DNode(T value) { 
+			item = value; 
+			next = nullptr; 
+			prev = nullptr; 
+		}
+
+		void show()const {
+			cout << "item: " << item << endl;
+			cout << "\tprev: " << prev << endl;
+			cout << "\tnext: " << next << endl;
+		}
+	};
 }
